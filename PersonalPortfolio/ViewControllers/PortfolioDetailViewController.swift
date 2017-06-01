089c63ba1acb6a723a0f8cd5549a87f8126f894d
ryanbrashear <ryan.brashear@interapthq.com>
2017-06-01 11:32:33 -0400

    Set up the portfolio details page to transfer to a full size image when the collection view item is tapped on. Also added a button to open the applications in the app store.

--- PersonalPortfolio/ViewControllers/PortfolioDetailViewController.swift
+++ PersonalPortfolio/ViewControllers/PortfolioDetailViewController.swift
@@ -13,25 +13,56 @@ class PortfolioDetailViewController: UIViewController, UICollectionViewDataSourc
     
     @IBOutlet weak var navItem: UINavigationItem!
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var appStoreButton: UIBarButtonItem!
     
     var portfolioItem: PortfolioItem!
+    var appStoreURL: URL!
+    var selectedImage: UIImage! {
+        didSet {
+            self.performSegue(withIdentifier: "showFullSizeImage", sender: self)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navItem.title = portfolioItem?.appName
+        descriptionTextView.text = portfolioItem.responsibilityDescription
         
         let layout = AnimatedCollectionViewLayout()
         layout.animator = PageAttributesAnimator()
         layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: collectionView.bounds.width * 0.85, height: collectionView.bounds.height * 0.80)
+        layout.itemSize = CGSize(width: view.frame.width, height: collectionView.bounds.height * 0.80)
         collectionView.collectionViewLayout = layout
+        
+        if portfolioItem.linkToAppStore == nil {
+            appStoreButton.isEnabled = false
+            appStoreButton.tintColor = UIColor.clear
+        } else {
+            appStoreURL = portfolioItem.linkToAppStore
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        descriptionTextView.setContentOffset(CGPoint.zero, animated: false)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? FullSizeImageViewController {
+            destination.image = selectedImage
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "screenshotCell", for: indexPath as IndexPath) as? ScreenshotCollectionViewCell
         cell!.imageView.image = portfolioItem.screenshots[indexPath.row]
         return cell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedImage = portfolioItem.screenshots[indexPath.row]
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -44,6 +75,13 @@ class PortfolioDetailViewController: UIViewController, UICollectionViewDataSourc
 
     @IBAction func backButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: "unwindToPortfolio", sender: self)
+    }
+    
+    @IBAction func appStoreButtonTapped(_ sender: Any) {
+        UIApplication.shared.open(appStoreURL)
+    }
+    
+    @IBAction func unwindToPortfolioDetail(segue: UIStoryboardSegue) {
     }
 }
 
--- PersonalPortfolio/ViewControllers/PortfolioViewController.swift
+++ PersonalPortfolio/ViewControllers/PortfolioViewController.swift
@@ -15,20 +15,21 @@ class PortfolioViewController: UIViewController, UICollectionViewDataSource, UIC
     @IBOutlet weak var pageControl: UIPageControl!
     
     var portfolioItems: [PortfolioItem] = []
-    var selectedPortfolioItem: PortfolioItem?
+    var selectedPortfolioItem: PortfolioItem? {
+        didSet {
+            performSegue(withIdentifier: "showPortfolioDetail", sender: self)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let hugItem = PortfolioItem(appName: "HUG", responsibilityDescription: "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.", logoImage: #imageLiteral(resourceName: "HUG"), linkToAppStore: URL(string: "https://itunes.apple.com/us/app/hug-reading-program/id1228314146?mt=8")!, screenshots: [#imageLiteral(resourceName: "hugHome"), #imageLiteral(resourceName: "hugSlideMenu"), #imageLiteral(resourceName: "hugStudentProgress"), #imageLiteral(resourceName: "hugSchoolDirectory")])
         
-        let chirpItem = PortfolioItem(appName: "CHIRP", responsibilityDescription: "Test", logoImage: #imageLiteral(resourceName: "CHIRP"), linkToAppStore: URL(string: "https://itunes.apple.com/us/app/hug-reading-program/id1228314146?mt=8")!, screenshots: [#imageLiteral(resourceName: "homeScreen"), #imageLiteral(resourceName: "nowPlaying"), #imageLiteral(resourceName: "recentlyPlayed"), #imageLiteral(resourceName: "favorites")])
-//        
-//        let kuncItem = PortfolioItem(appName: "KUNC", responsibilityDescription: "Test", logoImage: #imageLiteral(resourceName: "KUNC"), linkToAppStore: URL(string: "https://itunes.apple.com/us/app/hug-reading-program/id1228314146?mt=8")!)
+        let chirpItem = PortfolioItem(appName: "CHIRP", responsibilityDescription: "Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.", logoImage: #imageLiteral(resourceName: "CHIRP"), linkToAppStore: URL(string: "https://itunes.apple.com/us/app/chirp-radio/id373395037?mt=8")!, screenshots: [#imageLiteral(resourceName: "homeScreen"), #imageLiteral(resourceName: "nowPlaying"), #imageLiteral(resourceName: "recentlyPlayed"), #imageLiteral(resourceName: "favorites")])
         
         portfolioItems.append(hugItem)
         portfolioItems.append(chirpItem)
-//        portfolioItems.append(kuncItem)
         
         pageControl.pageIndicatorTintColor = UIColor(hex: "6ABBEA")
         pageControl.isUserInteractionEnabled = false
@@ -60,7 +61,6 @@ class PortfolioViewController: UIViewController, UICollectionViewDataSource, UIC
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         selectedPortfolioItem = portfolioItems[indexPath.row]
-        performSegue(withIdentifier: "showPortfolioDetail", sender: self)
     }
     
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
